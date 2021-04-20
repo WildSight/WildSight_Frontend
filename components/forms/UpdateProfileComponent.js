ba69f73afd813b01f18b37bb357e86cbc5370098
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-04-21 01:28:19 +0530

    update profile feature

--- components/forms/UpdateProfileComponent.js
+++ components/forms/UpdateProfileComponent.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import { View, ImageBackground, Text, Alert, StyleSheet, Dimensions, TouchableOpacity} from 'react-native';
 import { Button, Image, Input, Icon} from 'react-native-elements';
 import {getImageFromGallery} from '../commonComponents/permissions'
+import {updateUserProfile} from '../../redux/actions/user';
 
 var width = Math.floor(Dimensions.get('window').width/2);
 var heigth = Math.floor(Dimensions.get('window').height/3);
@@ -22,6 +24,7 @@ class UpdateUser extends Component {
             }
         }
     }
+
     formValidation = ()=>{
         const {email, password} = this.state;
         let emailError="", passwordError="", error=false;
@@ -32,7 +35,7 @@ class UpdateUser extends Component {
                 emailError="Enter a valid email";
             }
         }
-        if(!password && (String(password).length<4)){
+        if(password && (String(password).length<4)){
             error=true;
             passwordError="Password should be 4 or more characters long";
         }
@@ -45,29 +48,44 @@ class UpdateUser extends Component {
         return !error;
 
     }
-    handleSubmit= ()=>{
-        const {firstName, lastName, email, password, imageUrl} = this.state;
-        if(this.formValidation())
-            console.log("Data recorded");
+    handleSubmit= async()=>{
+        const token = this.props.auth.token;
+        const username = this.props.auth.username;
+        const {firstName, lastName, email, password} = this.state;
+        if(this.formValidation()&&(firstName||lastName||email||password)){
+            let data = {token, username}
+            firstName?data["first_name"]=firstName:null;
+            lastName?data["last_name"]=lastName:null;
+            email?data["email"]=email:null;
+            password?data["password"]=password:null;
+            await this.props.updateUserProfile(data);
+            if(this.props.UserProfile.errMess){
+                Alert.alert("Error occured", this.props.UserProfile.errMess);
+            }else if(this.props.UserProfile.data){
+                Alert.alert("Update Success", "Profile updated successfully");
+                this.props.navigation.navigate('MY PROFILE');
+            }
+        }
+            
     }
 
-    getGalleryImage = async () => {
-        var selectedImage  = await getImageFromGallery(width, heigth);
-        if(selectedImage){
-            if(!selectedImage.error){
-                this.setState({
-                    imageUrl: selectedImage.imageUrl,
-                    blob: selectedImage.blob
-                });
+    // getGalleryImage = async () => {
+    //     var selectedImage  = await getImageFromGallery(width, heigth);
+    //     if(selectedImage){
+    //         if(!selectedImage.error){
+    //             this.setState({
+    //                 imageUrl: selectedImage.imageUrl,
+    //                 blob: selectedImage.blob
+    //             });
     
-            }else{
-                console.log("Error", selectedImage.error);
-            }
-        }else{
-            console.log("Image not selected");
-        }
+    //         }else{
+    //             console.log("Error", selectedImage.error);
+    //         }
+    //     }else{
+    //         console.log("Image not selected");
+    //     }
         
-    } 
+    // } 
 
     render() {
         return (
@@ -75,7 +93,8 @@ class UpdateUser extends Component {
                 <ImageBackground source={require('../images/wild2.png')} style={styles.image}>
                     <View style={{height:'100%', backgroundColor: "#000000aa"}}>
 
-                    <View style={{marginHorizontal: '5%', marginTop:'10%'}}>
+                    <View style={{marginHorizontal: '5%', marginTop:'8%'}}>
+                    <Text style={{marginBottom:'5%', color:'red'}}>** Fill only those fields that you want to edit</Text>
                         <Input
                             placeholder="Enter First Name...."
                             leftIcon={{ type: 'font-awesome-5', name: 'user', color: '#11cbd7'}}
@@ -99,7 +118,7 @@ class UpdateUser extends Component {
                             style={{color: 'white'}}
                         />
                         <Input
-                            placeholder="Enter Email...."
+                            placeholder="Enter new Email...."
                             leftIcon={{ type: 'font-awesome-5', name: 'envelope', color: '#11cbd7'}}
                             leftIconContainerStyle={{marginRight: 10}}
                             onChangeText={(email) => this.setState({email})}
@@ -111,7 +130,7 @@ class UpdateUser extends Component {
                             style={{color: 'white'}}
                         />
                         <Input
-                            placeholder="Enter Password...."
+                            placeholder="Enter new Password...."
                             leftIcon={{ type: 'font-awesome-5', name: 'user-lock', color: '#11cbd7'}}
                             leftIconContainerStyle={{marginRight: 10}}
                             onChangeText={(password) => this.setState({password})}
@@ -123,7 +142,7 @@ class UpdateUser extends Component {
                             placeholderTextColor='white'
                             style={{color: 'white'}}
                         />
-                        <View style={{width: '45%', paddingLeft: '2%', marginTop:'5%', marginBottom:'3%'}}>
+                        {/* <View style={{width: '45%', paddingLeft: '2%', marginTop:'5%', marginBottom:'3%'}}>
                             <Button
                                 title="Upload Image"
                                 onPress={this.getGalleryImage}
@@ -136,12 +155,12 @@ class UpdateUser extends Component {
                                 {!this.state.imageUrl && <Text style={{marginTop:'2%', color:'orange'}}>No image selected</Text>}
                                 {this.state.imageUrl && <Text style={{marginTop:'2%', color:'green'}}>Image selected successfully</Text>}
                             </Text>
-                        </View>
+                        </View> */}
 
                         <View style={{marginHorizontal: '5%', marginVertical: '5%'}}>
                             <Button
                                 onPress = {() => this.handleSubmit()}
-                                title="Update User"
+                                title="Update Profile"
                                 titleStyle={{marginLeft: '5%'}}
                                 icon={
                                     <Icon
@@ -185,4 +204,14 @@ const styles = StyleSheet.create({
       }
 });
 
-export default UpdateUser; 
+const mapStateToProps = (state, ownProps)=>{
+    return({
+        ...ownProps,
+        auth: state.Auth,
+        UserProfile: state.UserProfile
+
+    })
+
+}
+
+export default connect(mapStateToProps,{updateUserProfile})(UpdateUser); 
